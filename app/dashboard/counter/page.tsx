f1d0c45aa160fc82e554d41049becf4ab9b68ea7
WillmanGZ <willmang@uninorte.edu.co>
2026-01-16 23:47:56 -0500

    Add sidebar component with menu items and implement counter functionality

--- app/dashboard/counter/page.tsx
+++ app/dashboard/counter/page.tsx
@@ -1,7 +1,16 @@
+import { Metadata } from "next";
+import CartCounter from "@/app/shopping-cart/components/CartCounter";
+
+export const metadata: Metadata = {
+  title: "Shopping Cart",
+  description: "Un Simple Contador",
+};
+
 export default function CounterPage() {
   return (
-    <div>
-      <h1>Counter Page</h1>
+    <div className="flex flex-col items-center justify-center w-full h-full">
+      <span>Productos en el carrito de compras</span>
+      <CartCounter value={20} />
     </div>
   );
 }
